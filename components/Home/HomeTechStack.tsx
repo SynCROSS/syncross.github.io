dd6004a98f8a6e88ba80a55fc2b0a69f4715691a
unknown <51585533+SynCROSS@users.noreply.github.com>
2021-03-08 16:00:23 +0900

    Update HomeTechStack.tsx

--- components/Home/HomeTechStack.tsx
+++ components/Home/HomeTechStack.tsx
@@ -1,53 +1,59 @@
 import styled from 'styled-components';
 
 const HomeTechStackBlock = styled.section`
-  width: 50%;
-  margin: 20px auto;
+	width: 50%;
+	margin: 20px auto;
 `;
 
 const TechStackTitle = styled.h1`
-  font-size: 3rem;
-  margin: 20px auto;
+	font-size: 3rem;
+	margin: 20px auto;
 `;
 
-const TechList = styled.div`
-  flex-wrap: wrap;
-  justify-content: space-evenly;
+const TechList = styled.ul`
+	flex-wrap: wrap;
+	list-style: none;
+`;
+
+const TechListItem = styled.ul`
+	border-radius: 50px;
+	background: #f8f8f8;
+	box-shadow: 10px 10px 20px #cccccc, -10px -10px 20px #ffffff;
+	padding: 20px;
+	margin: 1rem;
+	transition: all 0.2s ease-in-out;
+
+	&:hover {
+		background-color: #fcfcfc;
+		width: 20rem;
+	}
 `;
 
 const TechListImage = styled.img`
-  width: 170px;
-  height: 170px;
-  border-radius: 50%;
-  background: #f8f8f8;
-  box-shadow: 10px 10px 20px #cccccc, -10px -10px 20px #ffffff;
-  padding: 40px;
-  margin: 15px;
-  transition: all 0.2s ease-in-out;
-
-  &:hover {
-    background-color: #fcfcfc;
-  }
+	width: 100px;
+	height: 100px;
 `;
 
 const HomeTechStack = () => {
-  const techStacks = ['mongodb', 'express', 'react', 'node-dot-js'];
+	const techStacks = ['mongodb', 'express', 'react', 'node-dot-js'];
 
-  return (
-    <HomeTechStackBlock>
-      <TechStackTitle>Tech Stack</TechStackTitle>
-      <TechList className="flex ai-center">
-        {techStacks?.map(techStack => (
-          <TechListImage
-            key={`${techStack}`}
-            src={`https://cdn.jsdelivr.net/npm/simple-icons@v4/icons/${techStack}.svg`}
-            title={`${techStack}`}
-            alt={`${techStack}`}
-          />
-        ))}
-      </TechList>
-    </HomeTechStackBlock>
-  );
+	return (
+		<HomeTechStackBlock>
+			<TechStackTitle>Tech Stack</TechStackTitle>
+			<TechList className="flex flex-direction-col">
+				{techStacks?.map(techStack => (
+					<TechListItem>
+						<TechListImage
+							key={`${techStack}`}
+							src={`https://cdn.jsdelivr.net/npm/simple-icons@v4/icons/${techStack}.svg`}
+							title={`${techStack}`}
+							alt={`${techStack}`}
+						/>
+					</TechListItem>
+				))}
+			</TechList>
+		</HomeTechStackBlock>
+	);
 };
 
 export default HomeTechStack;
