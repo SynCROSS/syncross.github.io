c099cddc915c95b82243a73eecd6ddf5958eef7e
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-03-11 19:41:58 +0900

    For Code Conventions

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -17,26 +17,26 @@ const HeaderBlock = styled.div`
   left: 0;
   z-index: 10;
   padding: 15px 20px;
+`;
 
-  & > ul {
-    list-style: none;
-    color: gray;
-  }
+const IconList = styled.ul`
+  list-style: none;
+  color: gray;
+`;
 
-  & > ul > li {
-    display: inline-flex;
-    margin: 0 1rem;
-  }
+const IconItem = styled.li`
+  display: inline-flex;
+  margin: 0 1rem;
 
-  & > ul > li:hover {
+  &:hover {
     color: #000;
   }
+`;
 
-  .fa-icon {
-    font-size: 1.5rem;
-    color: gray;
-  }
-  .fa-icon:hover {
+const FontAwesomeIconBlock = styled(FontAwesomeIcon)`
+  font-size: 1.5rem;
+  color: gray;
+  &:hover {
     color: #505050;
   }
 `;
@@ -44,57 +44,61 @@ const HeaderBlock = styled.div`
 const Header = () => {
   return (
     <HeaderBlock className="flex ai-center">
-      <ul>
-        <li>
+      <IconList>
+        <IconItem>
           <Link href="/">
             <a>Home</a>
           </Link>
-        </li>
-        <li>
+        </IconItem>
+        <IconItem>
           <Link href="/About">
             <a>About</a>
           </Link>
-        </li>
-        <li>
+        </IconItem>
+        <IconItem>
           <Link href="/Work">
             <a>My Works</a>
           </Link>
-        </li>
-      </ul>
-      <ul>
-        <li>
+        </IconItem>
+      </IconList>
+      <IconList>
+        <IconItem>
           <a
             href="https://www.linkedin.com/in/%EC%A4%80%EC%84%9D-%EB%B0%95-4a9866194/?locale=en_US"
             target="_blank"
-            rel="noopener noreferrer">
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon icon={faLinkedin} className="fa-icon" />
           </a>
-        </li>
-        <li>
+        </IconItem>
+        <IconItem>
           <a
             href="https://twitter.com/1MD3V3L0P3R"
             target="_blank"
-            rel="noopener noreferrer">
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon icon={faTwitterSquare} className="fa-icon" />
           </a>
-        </li>
-        <li>
+        </IconItem>
+        <IconItem>
           <a
             href="http://github.com/SynCROSS"
             target="_blank"
-            rel="noopener noreferrer">
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon icon={faGithub} className="fa-icon" />
           </a>
-        </li>
-        <li>
+        </IconItem>
+        <IconItem>
           <a
             href="https://www.instagram.com/junseok3125/"
             target="_blank"
-            rel="noopener noreferrer">
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon icon={faInstagramSquare} className="fa-icon" />
           </a>
-        </li>
-      </ul>
+        </IconItem>
+      </IconList>
     </HeaderBlock>
   );
 };
