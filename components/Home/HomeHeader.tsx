83bc412ad20a76012ccf36adf8b5b97790334a4a
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-03-14 21:29:57 +0900

    Add Move Scroll To Bottom Button

--- components/Home/HomeHeader.tsx
+++ components/Home/HomeHeader.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowDown, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
 const HomeHeaderBlock = styled.header`
-  min-height: 100vh;
+  min-height: 95vh;
   min-width: 100vw;
   background: linear-gradient(165deg, #f5f5f5 50%, #fff 50%);
 `;
@@ -13,6 +13,7 @@ const HomeHeaderBlock = styled.header`
 const HeadLine = styled.h1`
   font-weight: 900;
   font-size: 5rem;
+
   margin: 0 auto 20px;
   line-height: 1.2;
 `;
@@ -28,25 +29,48 @@ const RouterLink = styled.a`
   color: #fff;
   border: none;
   border-radius: 50px;
+  font-size: 1.2rem;
+
+  cursor: pointer;
   padding: 1rem 40px 1rem 20px;
   position: relative;
   width: 10rem;
-  font-size: 1.2rem;
 `;
 
 const FontAwesomeIconBlock = styled(FontAwesomeIcon)`
   margin-left: 10px;
   position: absolute;
+  top: 50.5%;
+
   transition: all 0.15s ease-in-out;
   opacity: 0.5;
-  top: 50.5%;
   transform: translate(0, -50%);
+`;
+
+const ArrowButton = styled.button`
+  width: 5rem;
+  height: 5rem;
+  position: absolute;
+  top: 92vh;
+
+  font-size: 2rem;
+  color: #f8f8f8;
+  background-color: #000;
+  border: none;
+
+  &:hover {
+    background-color: #1c1c1c;
+    color: #fff;
+  }
 `;
 
 const HomeHeader = () => {
   const [right, setRight] = useState(false);
-
   const toggleRight = () => setRight(!right);
+
+  function movePresentScroll(event) {
+    window.scrollTo(event.pageX, event.pageY + 10);
+  }
 
   return (
     <HomeHeaderBlock className="flex jc-center ai-center flex-direction-col">
@@ -59,7 +83,6 @@ const HomeHeader = () => {
           My Work{' '}
           <FontAwesomeIconBlock
             icon={faArrowRight}
-            className="fa-icon"
             style={
               right && right
                 ? { right: '1.2rem', opacity: '1' }
@@ -68,6 +91,9 @@ const HomeHeader = () => {
           />
         </RouterLink>
       </Link>
+      <ArrowButton onClick={movePresentScroll}>
+        <FontAwesomeIcon icon={faArrowDown}></FontAwesomeIcon>
+      </ArrowButton>
     </HomeHeaderBlock>
   );
 };
