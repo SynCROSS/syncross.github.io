8cdb9af879d6203171082bedb8c7ec30252df6fb
unknown <51585533+SynCROSS@users.noreply.github.com>
2021-03-17 15:13:05 +0900

    Move meta tags index to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,25 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="keywords" content="syncross" />
+        <meta
+          name="description"
+          content="SynCROSS's Portfolio Site. This App Built with Nextjs and TypeScript. I Hope You Feel Good. :)"
+        />
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
