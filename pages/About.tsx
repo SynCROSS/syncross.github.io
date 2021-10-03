e0f4f9103f184ef13b667e6d4615833286964150
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-10-04 08:33:50 +0900

    Edit Font Size & Text of About Page

--- pages/About.tsx
+++ pages/About.tsx
@@ -13,12 +13,16 @@ const AboutBlock = styled.div`
 
 const AboutTitle = styled.h1`
   margin: 1rem;
-  font-size: 5rem;
+  font-size: 3rem;
+
+  @media only screen and (max-width: 600px) {
+    font-size: 2.4rem;
+  }
 `;
 
 const AboutContext = styled.p`
   text-align: left;
-  font-size: 2rem;
+  font-size: 1.5rem;
   margin: 1rem;
 
   @media only screen and (max-width: 600px) {
@@ -34,13 +38,26 @@ const About = () => {
       </Head>
       <AboutTitle>About</AboutTitle>
       <AboutContext>
-        This Is My Personal Site. The App Built With Next.js And Typescript. And
-        This Is My Portfolio And My First Personal Project. So I'm Very Excited
-        to Make This Site. Many People Might Be Wondering Why I Chose Next
-        Instead Of Gatsby to Make This Site. Because I Didn't Know Gatsby When I
-        Started This Project. Now I Already Know Gatsby But I Won't Change Next
-        Into Gatsby. Because I Will Add Dynamic Contents When I Have A Chance
-        Later. I Hope You Feel Good. :)
+        This App Built With Next.js And Typescript.
+        <br />
+        And This Is My Portfolio And My First Personal Project.
+        <br />
+        So I'm Very Excited to Make This Site.
+        <br />
+        Many People Might Be Wondering
+        <br />
+        Why I Chose Next Instead Of Gatsby to Make This Site.
+        <br />
+        Because I Didn't Know Gatsby When I Started This Project.
+        <br />
+        And Next.js Can Change Rendering Solution Between CSR & SSR.
+        <br />
+        Anyway,
+        <br />
+        If U Wanna Hire Some Frontend Engineers
+        <br />
+        Please Contact Me.
+        <br />I Hope You Feel So Good. (●'◡'●)
       </AboutContext>
     </AboutBlock>
   );
