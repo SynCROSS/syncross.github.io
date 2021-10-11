3dc3cfb7d5ab60728efa810e19b1a6a37e4ced77
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-10-11 18:03:04 +0900

    Remove 'keywords' & Add 'title' & Edit 'description'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,10 +13,10 @@ export default function App({ Component, pageProps }: AppProps) {
         ></meta>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="keywords" content="syncross" />
+        <meta name="title" content="SynCROSS" />
         <meta
           name="description"
-          content="SynCROSS's Portfolio. This App Built with Next.js and TypeScript. I Hope You Feel Good And Please Contact Me. (●'◡'●)"
+          content="SynCROSS's Portfolio. I'm MERN Stack Developer. I Hope You Feel Good And Please Contact Me. (●'◡'●)"
         />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
       </Head>
