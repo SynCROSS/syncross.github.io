38b049cd4a6c21041acc5e0de0f6fc69491e8801
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-10-24 15:48:01 +0900

    Add 'lang' Attribute To <html>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
 import HomeHeader from '../components/Home/HomeHeader';
 import HomeIntroduce from '../components/Home/HomeIntroduce';
 import HomeTechStack from '../components/Home/HomeTechStack';
 
 const Home = () => {
+  useEffect(() => {
+    try {
+      if (typeof document !== 'undefined' && typeof navigator !== 'undefined') {
+        document.documentElement.lang = navigator.language;
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }, []);
   return (
     <div className="flex jc-center ai-center flex-direction-col">
       <Head>
