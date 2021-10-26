53cef558bb59c5a09a03d77161ef5c8ef3465d11
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-10-26 10:17:21 +0900

    Add 'aria-label'

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -163,6 +163,7 @@ const Header = () => {
               href="https://www.linkedin.com/in/%EC%A4%80%EC%84%9D-%EB%B0%95-4a9866194/?locale=en_US"
               target="_blank"
               rel="noopener noreferrer"
+              aria-label="LinkedIn"
             >
               <FontAwesomeIconBlock icon={faLinkedin} />
             </a>
@@ -172,6 +173,7 @@ const Header = () => {
               href="https://twitter.com/1MD3V3L0P3R"
               target="_blank"
               rel="noopener noreferrer"
+              aria-label="Twitter"
             >
               <FontAwesomeIconBlock icon={faTwitterSquare} />
             </a>
@@ -181,6 +183,7 @@ const Header = () => {
               href="http://github.com/SynCROSS"
               target="_blank"
               rel="noopener noreferrer"
+              aria-label="Github"
             >
               <FontAwesomeIconBlock icon={faGithub} />
             </a>
@@ -190,6 +193,7 @@ const Header = () => {
               href="https://www.instagram.com/junseok3125/"
               target="_blank"
               rel="noopener noreferrer"
+              aria-label="Instagram"
             >
               <FontAwesomeIconBlock icon={faInstagramSquare} />
             </a>
