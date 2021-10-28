214b48788cad7170047f4e20c4435fcf0f858604
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-10-28 09:03:11 +0900

    Add <meta> About Open Graph

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,21 +4,40 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const TITLE = 'SynCROSS';
+  const DESCRIPTION =
+    "I'm MERN Stack Developer. I Hope You Feel Good And Please Contact Me. (●'◡'●)";
+
   return (
     <Layout>
       <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="title" content="SynCROSS" />
-        <meta
-          name="description"
-          content="SynCROSS's Portfolio. I'm MERN Stack Developer. I Hope You Feel Good And Please Contact Me. (●'◡'●)"
-        />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="title" content={`${TITLE}`} />
+        <meta name="description" content={`${DESCRIPTION}`} key="desc" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={`${TITLE}`} />
+        <meta property="og:description" content={`${DESCRIPTION}`} />
+
+        <meta
+          property="og:image"
+          content="https://syncross.vercel.app/icon.jpg"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://syncross.vercel.app/icon.jpg"
+        />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="721" />
+        <meta property="og:image:height" content="721" />
+        <meta property="og:image:alt" content="SynCROSS" />
+
+        <meta property="og:url" content="https://syncross.vercel.app" />
+
+        <link rel="canonical" href="https://syncross.vercel.app/" />
       </Head>
       <Component {...pageProps} />
     </Layout>
