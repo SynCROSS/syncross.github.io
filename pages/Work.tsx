1f7d3b876e55f5a8e00d35f41b451e9cef7b2fab
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-10-30 22:35:55 +0900

    Divide Codes About Theme

--- pages/Work.tsx
+++ pages/Work.tsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 import Head from 'next/head';
+import Image from 'next/image';
+import { getRandomTheme } from '../lib/theme/GithubRepository';
+import { useCallback } from 'react';
 
 const WorkBlock = styled.div`
   align-items: baseline;
@@ -9,84 +12,14 @@ const WorkBlock = styled.div`
 const Work = () => {
   const githubRepoArray = ['nextjs-typescript-setting'];
 
-  const generateURL = (repo: string) => {
-    if (!repo) {
-      return '';
-    }
-
-    const getRandomTheme = () => {
-      const themes = [
-        'default',
-        'default_repocard',
-        'dark',
-        'radical',
-        'merko',
-        'gruvbox',
-        'gruvbox_light',
-        'tokyonight',
-        'onedark',
-        'cobalt',
-        'synthwave',
-        'highcontrast',
-        'dracula',
-        'prussian',
-        'monokai',
-        'vue',
-        'vue-dark',
-        'shades-of-purple',
-        'nightowl',
-        'buefy',
-        'blue-green',
-        'algolia',
-        'great-gatsby',
-        'darcula',
-        'bear',
-        'solarized-dark',
-        'solarized-light',
-        'chartreuse-dark',
-        'nord',
-        'gotham',
-        'material-palenight',
-        'graywhite',
-        'vision-friendly-dark',
-        'ayu-mirage',
-        'midnight-purple',
-        'calm',
-        'flag-india',
-        'omni',
-        'react',
-        'jolly',
-        'maroongold',
-        'yeblu',
-        'blueberry',
-        'slateorange',
-        'kacho_ga',
-        'outrun',
-        'ocean_dark',
-        'city_lights',
-        'github_dark',
-        'discord_old_blurple',
-        'aura_dark',
-        'panda',
-        'noctis_minimus',
-        'cobalt2',
-      ];
-
-      return themes.sort(() => Math.random() - 0.5)[
-        Math.floor(Math.random() * themes.length)
-      ];
-    };
-
-    const GITHUB_USERNAME = 'SynCROSS';
-
-    return (
-      `https://github-readme-stats.vercel.app/api/pin/` +
+  const GITHUB_USERNAME = 'SynCROSS';
+  const getRepoImgURL = (repo: string): string =>
+    repo &&
+    `https://github-readme-stats.vercel.app/api/pin/` +
       '' +
       `?username=${GITHUB_USERNAME}&repo=${repo}&theme=${getRandomTheme()}` +
       '' +
-      `&hide_border=true&show_icons=true&count_private=true`
-    );
-  };
+      `&hide_border=true&show_icons=true&count_private=true`;
 
   return (
     <WorkBlock>
@@ -98,8 +31,15 @@ const Work = () => {
       <div style={{ margin: '2rem auto' }}>
         <ul className="flex flex-direction-col jc-center ai-center">
           {githubRepoArray?.map?.(repo => (
-            <li style={{ margin: '5px auto' }}>
-              <img src={generateURL(repo)} alt={`${repo}`} />
+            <li key={repo} style={{ margin: '5px auto' }}>
+              <Image
+                src={getRepoImgURL(`${repo}`)}
+                // sizes={'2w'}
+                // layout={'responsive'}
+                width={300}
+                height={90}
+                quality={100}
+              />
             </li>
           ))}
         </ul>
