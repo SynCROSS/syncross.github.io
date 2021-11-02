2621ef1f84da27981889510c9e946ceb28632e8e
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-11-02 17:14:24 +0900

    Set Header Config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,50 @@
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value:
+      `default-src 'self' https://fonts.googleapis.com/ 'unsafe-inline'; ` +
+      `object-src 'none'; ` +
+      `report-uri 'none'; ` +
+      `script-src 'self' 'unsafe-eval'; ` +
+      `script-src-elem 'self' 'unsafe-inline'; ` +
+      `font-src https://fonts.googleapis.com/  https://fonts.gstatic.com/; ` +
+      `style-src-elem 'self' https://fonts.googleapis.com/ 'unsafe-inline'; ` +
+      `img-src 'self' data:; ` +
+      `frame-ancestors 'self'`,
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+];
+
 module.exports = {
   images: {
     domains: ['unpkg.com', 'github-readme-stats.vercel.app'],
+  },
+  async headers() {
+    return [
+      {
+        // * Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
   },
 };
