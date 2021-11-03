cda0bb41535ba3f06e527e34e0da7e0f6b3465bf
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-11-03 09:36:50 +0900

    Remove <meta http-equiv="Content-Security-Policy"/>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,10 +23,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
-        <meta
-          httpEquiv="Content-Security-Policy"
-          content="default-src 'self' 'unsafe-inline'; script-src 'unsafe-eval'; script-src-elem 'self' 'unsafe-inline'; font-src https://fonts.googleapis.com/  https://fonts.gstatic.com/; style-src-elem 'self' https://fonts.googleapis.com/ 'unsafe-inline'; img-src 'self' data:"
-        />
+
         <meta name="title" content={`${TITLE}`} />
         <meta name="description" content={`${DESCRIPTION}`} key="desc" />
 
@@ -48,6 +45,8 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta property="og:image:alt" content="SynCROSS" />
 
         <meta property="og:url" content="https://syncross.vercel.app" />
+
+        <meta name="twitter:card" content="summary" />
 
         <link
           href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap"
