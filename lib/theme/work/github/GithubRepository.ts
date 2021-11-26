382f6775f5058236b562eb62cc6bccfbac3729f5
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-11-26 12:25:52 +0900

    Edit Shuffle Logic, etc . . .

--- lib/theme/work/github/GithubRepository.ts
+++ lib/theme/work/github/GithubRepository.ts
@@ -1,68 +1,71 @@
-function GithubRepositoryTheme() {
-  return [
-    'default',
-    'default_repocard',
-    'dark',
-    'radical',
-    'merko',
-    'gruvbox',
-    'gruvbox_light',
-    'tokyonight',
-    'onedark',
-    'cobalt',
-    'synthwave',
-    'highcontrast',
-    'dracula',
-    'prussian',
-    'monokai',
-    'vue',
-    'vue-dark',
-    'shades-of-purple',
-    'nightowl',
-    'buefy',
-    'blue-green',
-    'algolia',
-    'great-gatsby',
-    'darcula',
-    'bear',
-    'solarized-dark',
-    'solarized-light',
-    'chartreuse-dark',
-    'nord',
-    'gotham',
-    'material-palenight',
-    'graywhite',
-    'vision-friendly-dark',
-    'ayu-mirage',
-    'midnight-purple',
-    'calm',
-    'flag-india',
-    'omni',
-    'react',
-    'jolly',
-    'maroongold',
-    'yeblu',
-    'blueberry',
-    'slateorange',
-    'kacho_ga',
-    'outrun',
-    'ocean_dark',
-    'city_lights',
-    'github_dark',
-    'discord_old_blurple',
-    'aura_dark',
-    'panda',
-    'noctis_minimus',
-    'cobalt2',
-  ];
-}
+const GithubRepoThemes = [
+  'default',
+  'default_repocard',
+  'dark',
+  'radical',
+  'merko',
+  'gruvbox',
+  'gruvbox_light',
+  'tokyonight',
+  'onedark',
+  'cobalt',
+  'synthwave',
+  'highcontrast',
+  'dracula',
+  'prussian',
+  'monokai',
+  'vue',
+  'vue-dark',
+  'shades-of-purple',
+  'nightowl',
+  'buefy',
+  'blue-green',
+  'algolia',
+  'great-gatsby',
+  'darcula',
+  'bear',
+  'solarized-dark',
+  'solarized-light',
+  'chartreuse-dark',
+  'nord',
+  'gotham',
+  'material-palenight',
+  'graywhite',
+  'vision-friendly-dark',
+  'ayu-mirage',
+  'midnight-purple',
+  'calm',
+  'flag-india',
+  'omni',
+  'react',
+  'jolly',
+  'maroongold',
+  'yeblu',
+  'blueberry',
+  'slateorange',
+  'kacho_ga',
+  'outrun',
+  'ocean_dark',
+  'city_lights',
+  'github_dark',
+  'discord_old_blurple',
+  'aura_dark',
+  'panda',
+  'noctis_minimus',
+  'cobalt2',
+];
 
-export function getRandomTheme(): string {
-  const themes = GithubRepositoryTheme();
+const getGithubRepoThemes = () => Object.assign([], GithubRepoThemes);
 
-  return themes.sort(() => Math.random() - 0.5)[
-    Math.floor(Math.random() * themes.length)
-  ];
-}
+export const getRandomTheme = (): string => {
+  const themes = getGithubRepoThemes();
 
-export default GithubRepositoryTheme;
+  for (let i = themes.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [themes[i], themes[j]] = [themes[j], themes[i]];
+  }
+
+  return themes[Math.floor(Math.random() * themes?.length)];
+};
+
+export default getGithubRepoThemes;
