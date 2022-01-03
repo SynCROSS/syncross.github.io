3dcc138c5cccc9fbb97ff8df86cb8ecb63e8a6b1
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2022-01-03 15:32:22 +0900

    Add '<noscript />' Tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -121,6 +121,7 @@ export default function App({ Component, pageProps }: AppProps) {
         />
       </Head>
       <Component {...pageProps} />
+      <noscript>Your Browser Is Too Old To Use Javascript</noscript>
     </Layout>
   );
 }
