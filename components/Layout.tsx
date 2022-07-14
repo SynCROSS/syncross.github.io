f5bc831bed549d746eb14525db53c9031a92c8e1
SynCROSS <kuuhaku3125@gmail.com>
2022-07-14 22:24:11 +0900

    Add Type For Props

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,10 @@
+import type { PropsWithChildren } from 'react';
 import Header from './common/Header';
 import Footer from './common/Footer';
 
-function Layout({ children }) {
+type LayoutProps<T = Record<string, unknown>> = PropsWithChildren<T>;
+
+function Layout({ children }: LayoutProps): JSX.Element {
   return (
     <div>
       <Header />
