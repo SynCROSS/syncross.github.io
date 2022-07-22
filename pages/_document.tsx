cdaf9544b82ece85a2b8379431ca2eaebd3bf233
SynCROSS <kuuhaku3125@gmail.com>
2022-07-22 15:40:59 +0900

    Update For Next.js 12

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,40 +1,31 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import type { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext,
-  ): Promise<DocumentInitialProps> {
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx?.renderPage;
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
-        originalRenderPage?.({
-          enhanceApp: App => props =>
-            sheet?.collectStyles?.(<App {...props} />),
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
         });
 
-      const initialProps = await Document?.getInitialProps?.(ctx);
-
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
-        styles: (
-          <>
-            {initialProps?.styles}
-            {sheet?.getStyleElement?.()}
-          </>
-        ),
+        styles: [initialProps.styles, sheet.getStyleElement()],
       };
-    } catch (e) {
-      console.error(e);
     } finally {
-      sheet?.seal?.();
+      sheet.seal();
     }
-    return {
-      html: '',
-    };
   }
 
   render(): JSX.Element {
@@ -66,5 +57,3 @@ class MyDocument extends Document {
     );
   }
 }
-
-export default MyDocument;
