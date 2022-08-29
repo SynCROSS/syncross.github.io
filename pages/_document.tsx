cd68ba1ef148684121e704b976dba53b841acdfe
SynCROSS <kuuhaku3125@gmail.com>
2022-08-29 20:49:27 +0900

    Add Return Type

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,13 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document';
+import type { DocumentInitialProps } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
