4fffc0e2792c31d9e1948358ffd782876937b35d
SynCROSS <kuuhaku3125@gmail.com>
2022-09-25 17:52:54 +0900

    Use Font In Local

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -53,23 +53,7 @@ export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="en-US">
-        <Head>
-          <link
-            rel="preconnect"
-            href="https://fonts.googleapis.com"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="anonymous"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap&family=Poppins&display=swap"
-            rel="stylesheet preload prefetch"
-            as="style"
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
