270737e60e9e91914edf19e31ff655d8603f5967
SynCROSS <kuuhaku3125@gmail.com>
2022-09-25 17:57:51 +0900

    Fix Error
     A document must not include both a meta element with an http-equiv attribute whose value is content-type, and a meta element with a charset attribute.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -45,7 +45,6 @@ export default function App({ Component }: AppProps): JSX.Element {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
 
         <meta name="title" content={`${TITLE}`} />
         <meta name="description" content={`${DESCRIPTION}`} key="desc" />
