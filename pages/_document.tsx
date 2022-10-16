c972261cda085d58b4808855e582e58948f53776
SynCROSS <kuuhaku3125@gmail.com>
2022-10-16 19:09:18 +0900

    Add 'type' Prop

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -96,6 +96,7 @@ export default class MyDocument extends Document {
               rel="preload"
               href={`fonts/${font.split('-')[0]}/${font}`}
               as="font"
+              type="font/tff"
               crossOrigin=""
             />
           ))}
