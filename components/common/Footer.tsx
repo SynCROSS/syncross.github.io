549a9e6329bb4b6f1606d5a8687f0666cc80d21f
SynCROSS <kuuhaku3125@gmail.com>
2023-03-07 10:59:52 +0900

    Add Margin At <p>

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -6,8 +6,6 @@ const year = new Date().getFullYear();
 
 const { className, styles } = css.resolve`
   a {
-    margin: 1rem 0;
-
     color: #ff6683;
   }
   @media (hover: hover) {
@@ -20,24 +18,29 @@ const { className, styles } = css.resolve`
 // skipcq: JS-D1001
 function Footer() {
   return (
-    <footer className="flex jc-center ai-center footer-block">
-      Copyright &copy;&nbsp;
-      <span className="year">{year}</span>
-      &nbsp;Made by&nbsp;
-      <Link
-        className={className}
-        href="https://github.com/SynCROSS"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        SynCROSS
-      </Link>
+    <footer className="footer-block">
+      <p className="copyright">
+        Copyright &copy;&nbsp;
+        <span className="year">{year}</span>
+        &nbsp;Made by&nbsp;
+        <Link
+          className={className}
+          href="https://github.com/SynCROSS"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          SynCROSS
+        </Link>
+      </p>
       <style jsx>
         {`
           .footer-block {
             width: 100%;
             background-color: #000;
             color: #eee;
+          }
+          .copyright {
+            margin: 1rem auto;
           }
         `}
       </style>
