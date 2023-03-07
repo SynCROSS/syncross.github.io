bc56d0f560a99b6cf800fa38294fcc695b5bb604
SynCROSS <kuuhaku3125@gmail.com>
2023-03-07 11:25:58 +0900

    Move Styles To CSS File

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,11 @@
 import Layout from 'components/Layout';
-import type { AppProps, NextWebVitalsMetric } from 'next/app';
+import type { AppProps } from 'next/app';
 import { Poppins } from 'next/font/google';
 
 import { ViewportProvider } from 'lib/utility/viewport';
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
-
-/**
- * Log Metrics
- * @param metric Metrics Of Website
- */
-export function reportWebVitals(metric: NextWebVitalsMetric): void {
-  console.log('file: _app.tsx ~ line 12 ~ reportWebVitals ~ metric', metric);
-}
+import 'styles/global.css';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -26,74 +19,8 @@ function App({ Component }: AppProps) {
       <Layout>
         {/* skipcq: JS-0455 */}
         <style jsx global>{`
-          *,
-          *:before,
-          *:after {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-          }
           html {
             font-family: ${poppins.style.fontFamily};
-            scroll-behavior: smooth;
-          }
-
-          body {
-            height: 100%;
-            min-height: 100vh;
-            text-align: center;
-          }
-
-          a {
-            color: inherit;
-            text-decoration: none;
-          }
-
-          button {
-            cursor: pointer;
-            border: none;
-          }
-
-          ul {
-            list-style: none;
-          }
-
-          * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-          }
-
-          *::-webkit-scrollbar {
-            opacity: 0;
-            width: 0;
-            height: 0;
-          }
-
-          .flex {
-            display: flex;
-          }
-
-          .flex-direction-col {
-            flex-direction: column;
-          }
-
-          .ai-center {
-            align-items: center;
-          }
-
-          .ai-baseline {
-            align-items: baseline;
-          }
-
-          .jc-center {
-            justify-content: center;
-          }
-
-          #__next {
-            display: flex;
-            min-height: 100vh;
-            flex-direction: column;
           }
         `}</style>
         <Component />
