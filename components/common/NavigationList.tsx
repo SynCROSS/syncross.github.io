7a4d644f8c3cd0480b0fa6e798ce4f5dfa6596e4
SynCROSS <kuuhaku3125@gmail.com>
2023-08-09 21:48:15 +0900

    Restyle NavigationList With Tailwind

--- components/common/NavigationList.tsx
+++ components/common/NavigationList.tsx
@@ -1,22 +1,43 @@
+import classNames from 'classnames';
 import { bindStyle } from 'lib/utility/style';
 import Link from 'next/link';
+import { Children } from 'react';
 import styles from 'styles/common/NavigationList.module.css';
 
 const cx = bindStyle(styles);
 
+const navigationMap = {
+  '/': 'Home',
+  '/about': 'About',
+  '/work': 'My Works',
+} as const;
+
 // skipcq: JS-D1001
 function NavigationList() {
   return (
-    <ul className={cx('item-list')}>
-      <li className={cx('link-item')}>
-        <Link href="/">Home</Link>
-      </li>
-      <li className={cx('link-item')}>
-        <Link href="/about">About</Link>
-      </li>
-      <li className={cx('link-item')}>
-        <Link href="/work">My Works</Link>
-      </li>
+    <ul
+      className={classNames(
+        'inline-flex',
+        'max-sm:flex-1 max-sm:justify-center max-sm:items-center max-sm:flex-col',
+        'min-[2560px]:text-2xl',
+        'min-[3840px]:text-4xl',
+      )}
+    >
+      {Children.toArray(
+        Object.entries(navigationMap).map(([href, title]) => (
+          <li
+            className={classNames(
+              'motion-safe:transition-all hover:text-neutral-600 font-medium',
+              'max-sm:text-neutral-300 max-sm:my-2 max-sm:mx-0',
+              'sm:my-0 sm:mx-2 sm:text-neutral-500',
+              'md:mx-4',
+              'min-[3840px]:mx-6',
+            )}
+          >
+            <Link href={href}>{title}</Link>
+          </li>
+        )),
+      )}
     </ul>
   );
 }
