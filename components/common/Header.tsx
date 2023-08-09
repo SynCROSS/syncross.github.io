1b93cd0ab11e7cc212b9733b78d1fccaccefffa3
SynCROSS <kuuhaku3125@gmail.com>
2023-08-09 21:47:56 +0900

    Restyle Header With Tailwind

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,10 +1,8 @@
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
 import NavigationList from 'components/common/NavigationList';
 import SnsNavigationList from 'components/common/SnsNavigationList';
-
-import { bindStyle } from 'lib/utility/style';
-import styles from 'styles/common/Header.module.css';
 
 type HeaderProps = {
   isOpened: boolean;
@@ -13,27 +11,34 @@ type HeaderProps = {
   closeMenu: VoidFunction;
 };
 
-const cx = bindStyle(styles);
-
 // skipcq: JS-D1001
 function Header({ isOpened, width, openMenu, closeMenu }: HeaderProps) {
   return (
     <>
-      {width < 768 && (
+      {width < 640 && (
         <button
-          className={cx('menu-button')}
+          className={classNames(
+            'w-5 h-5 fixed top-4 left-3 z-40 bg-transparent leading-4',
+            "before:content-['☰'] before:text-gray-400 before:text-4xl/4",
+          )}
           title="Menu Icon"
           onClick={openMenu}
         />
       )}
       <header
-        className={cx('flex', 'ai-center', 'header', {
-          open: isOpened,
-        })}
+        className={classNames(
+          'flex items-center motion-safe:transition-all z-50 fixed top-0 left-0 justify-between flex-col',
+          'max-[640px]:w-0 max-[640px]:h-full max-[640px]:p-0 max-[640px]:invisible max-[640px]:bg-neutral-950 max-[640px]:[&.open]:w-28 max-[640px]:[&.open]:visible',
+          'sm:w-full sm:py-2 sm:px-4 sm:flex-row sm:bg-transparent',
+          'md:py-4 md:px-8',
+          {
+            open: isOpened,
+          },
+        )}
       >
-        {width < 768 && isOpened && (
+        {width < 640 && isOpened && (
           <button
-            className={cx('close-button')}
+            className={'bg-transparent text-gray-300 mt-4'}
             title="Close Menu Button"
             onClick={closeMenu}
           >
@@ -43,11 +48,15 @@ function Header({ isOpened, width, openMenu, closeMenu }: HeaderProps) {
         <NavigationList />
         <SnsNavigationList />
       </header>
-      {width < 768 && isOpened && (
+      {width < 640 && isOpened && (
         <div
           role="button"
           tabIndex={-1}
-          className={cx('backdrop', { open: isOpened })}
+          className={classNames(
+            'motion-safe:transition-all w-0 fixed inset-0 z-40 bg-black/70',
+            '[&.open]:w-full',
+            { open: isOpened },
+          )}
           onClick={closeMenu}
           aria-hidden="true"
         />
